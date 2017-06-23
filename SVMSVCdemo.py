0af73d8df6a154268061ee9215b4ff605a684769
YangZhendong <404013419@qq.com>
2017-06-22 21:30:28 -0600

    new model

--- SVMSVCdemo.py
+++ SVMSVCdemo.py
@@ -5,7 +5,7 @@ from sklearn.externals import joblib
 import time
 data = []
 labels = []
-with open("ATM/ATM1.txt") as file:
+with open("ATM/ATM12.txt") as file:
     for line in file:
         tokens = line.strip().split(' ')
         data.append([float(tk) for tk in tokens[:-1]])
@@ -21,7 +21,7 @@ print("读取输出为：", type(y))
 print(y)
 start = time.time()
 clf_linear = SVC(kernel='linear').fit(X, y)
-joblib.dump(clf_linear, "model/model_ATM5.m")
+joblib.dump(clf_linear, "model/model_linear_ATM12.m")
 print("预测结果为：", clf_linear.predict([[91	, 93.41, 624.14], [95, 95.66, 546.82]]))
 print("预测结果为：", clf_linear.predict([[103, 95.15, 39328.5], [112, 88.55, 5000]]))
 print("预测结果为：", clf_linear.predict([[76, 98.68, 53267.82], [582, 80, 99999]]))
@@ -29,23 +29,21 @@ print("预测结果为：", clf_linear.predict([[47, 100, 16256411.17], [65, 56.
 end = time.time()
 print("time:", end - start)
 
-"""
+
 start = time.time()
-clf = SVC().fit(X, y)
-print(clf.predict([[89.09, 116.16], [89, 5000], [95, 500], [82, 584]]))
+clf_rbf = SVC().fit(X, y)
+joblib.dump(clf_rbf, "model/model_rbf_ATM12.m")
 end = time.time()
 print("time:", end - start)
 
 start = time.time()
 clf_poly = SVC(kernel='poly', degree=3).fit(X, y)
-print(clf_poly.predict([[89.09, 116.16], [89, 5000], [95, 500], [82, 584]]))
+joblib.dump(clf_poly, "model/model_poly_ATM12.m")
 end = time.time()
 print("time:", end - start)
 
 start = time.time()
 clf_sigmoid = SVC(kernel='sigmoid').fit(X, y)
-print("预测结果为：", clf_linear.predict([[91	, 93.41, 624.14], [76, 98.68, 53267.82], [103, 95.15, 39328.5], [47, 100, 16256411.17]]))
-print("预测结果为：", clf_linear.predict([[65	, 56.45, 5000]]))
+joblib.dump(clf_sigmoid, "model/model_sigmoid_ATM12.m")
 end = time.time()
 print("time:", end - start)
-"""
