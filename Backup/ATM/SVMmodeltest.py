b2ac3dd26f5b0615c5856a7ff681191e03f83298
YangZhendong <404013419@qq.com>
2017-07-12 11:15:36 -0600

    2 cam predict success rate 74% improve the accuracy

---
+++ Backup/ATM/SVMmodeltest.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn.externals import joblib
+import matplotlib.pyplot as plt
+data = []
+with open("ATM/ATM34_time_test.txt") as file:
+    for line in file:
+        tokens = line.strip().split(' ')
+        data.append([float(tk) for tk in tokens])
+test_X = np.array(data)
+print("测试输入为：", test_X)
+clf_linear = joblib.load("model/model_linear_ATM12_time.m")
+test_X_result = clf_linear.predict(test_X)
+with open("ATM/result/ATM34_test_result_linear_ATM12_time.txt", 'w') as file2:
+    for line in test_X_result:
+        file2.write(line)
+        file2.write('\n')
+
+"""
+# 读取成功率和时延作图
+data_plot = []
+y_plot = []
+with open("ATM/ATM2.txt") as file:
+    for line in file:
+        tokens = line.strip().split(' ')
+        data_plot.append([float(tk) for tk in tokens[1:3]])
+        y_plot.append(tokens[-1])
+plot_X = np.array(data_plot)
+plot_y = np.array(y_plot)
+print("画图输入为：", type(plot_X), plot_X)
+print("画图输出为：", type(plot_y), plot_y)
+
+h = .02  # step size in the mesh
+# create a mesh to plot in
+x_min, x_max = plot_X[:, 0].min() - 1, plot_X[:, 0].max() + 1
+y_min, y_max = plot_X[:, 1].min() - 1, plot_X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+# title for the plots
+title = ['SVC with linear kernel']
+
+for i, clf in enumerate((clf_linear),):
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    plt.subplot(2, 2, i + 1)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+
+    # Plot also the training points
+    plt.scatter(plot_X[:, 0], plot_X[:, 1], c=plot_y, cmap=plt.cm.coolwarm)
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.xticks(())
+    plt.yticks(())
+    plt.title(title)
+plt.show()
+"""
---
+++ Backup/Other Tracking Method/videoentropy.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# coding=utf-8
+import numpy as np
+import cv2
+import time
+from PIL import Image
+
+
+def entropy(band):
+    hist, _ = np.histogram(band, bins=range(0, 256))
+    hist = hist[hist > 0]
+    return -np.log2(hist / hist.sum()).sum()
+
+
+def show_entropy(band_name, band):
+    bits = entropy(band)
+    per_pixel = bits / band.size
+    print("{:3s} entropy = {:.2f} bits, {:.6f} per pixel"
+          .format(band_name, bits, per_pixel))
+
+
+def process(img_file):
+    im = Image.open(img_file)
+    print(img_file, im.format, im.size, im.mode)
+    print()
+
+    rgb = im.convert("RGB")
+    hsv = im.convert("HSV")
+    grey = im.convert("L")
+    r, g, b = [np.asarray(component) for component in rgb.split()]
+    h, s, v = [np.asarray(component) for component in hsv.split()]
+    l = np.asarray(grey)
+
+    show_entropy("R", r)
+    show_entropy("G", g)
+    show_entropy("B", b)
+    print()
+
+    show_entropy("H", h)
+    show_entropy("S", s)
+    show_entropy("V", v)
+    print()
+
+    show_entropy("L", l)
+    print()
+
+
+def process_rgb_average(img_file):
+    im = Image.open(img_file)
+    print("Processing image:",img_file)
+    rgb = im.convert("RGB")
+    r, g, b = [np.asarray(component) for component in rgb.split()]
+    RGB_average = (entropy(r)+entropy(g)+entropy(b))/3
+    print("Entropy:", RGB_average)
+
+
+def process_rgb_delta(img_file, entropy_last_inner):
+    # im = Image.open(img_file)  # <class 'PIL.JpegImagePlugin.JpegImageFile'>
+    # print("Processing image:", img_file)
+    # rgb = im.convert("RGB")  # <class 'PIL.Image.Image'>
+    # r, g, b = [np.asarray(component) for component in rgb.split()] # <PIL.Image.Image image mode=L size=1280x720 at>
+    b, g, r = cv2.split(frame)
+    rgb_average = (entropy(r)+entropy(g)+entropy(b))/3
+    jitter = abs(rgb_average - entropy_last_inner)
+    print("Entropy Jitter:", jitter)
+    return rgb_average
+
+if __name__ == "__main__":
+
+    start = time.time()
+    rval = True
+    totalFrameNumber = 0
+    # 读入视频文件
+    vc = cv2.VideoCapture('video/final.mp4')
+    # 判断是否正常打开
+    if vc.isOpened():
+        totalFrameNumber = vc.get(cv2.CAP_PROP_FRAME_COUNT)
+        # 输出视频总帧数
+        print("Total frame number:", totalFrameNumber)
+    else:
+        rval = False
+        print(rval)
+    # 开始，结束帧
+    frame_start = 0
+    frame_stop = totalFrameNumber - 1
+    frame_now = frame_start
+    entropy_now = 0
+    while rval:
+        vc.set(cv2.CAP_PROP_POS_FRAMES, frame_now)
+        rval, frame = vc.read(q)  # frame: numpy.ndarray
+        print(rval)
+        # 显示视频，按Q退出
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        entropy_now = process_rgb_delta(frame, entropy_now)
+        # 每n秒获取一帧
+        frame_now = frame_now + 29
+        if frame_now > frame_stop:
+            rval = False
+
+    end = time.time()
+    print("time:", end - start)
+    vc.release()
+    cv2.destroyAllWindows()
